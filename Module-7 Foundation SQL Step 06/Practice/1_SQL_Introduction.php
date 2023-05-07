8aaf33d37b7441c61e5df3233b8e8d5a65fa6908
junaid-mij <junath57@gmail.com>
2023-05-07 14:30:28 +0600

    new

---
+++ Module-7 Foundation SQL Step 06/Practice/1_SQL_Introduction.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * MySQL        : MySQL is a popular open-source relational database management system (RDBMS) that is used for creating, storing, and managing 
+ *                data in databases. It is a powerful and scalable database solution that is used by many web applications to store and retrieve
+ *                data efficiently.
+ * 
+ * 
+ */
+
+echo "১) এসকিউএল নিয়ে কথাবার্তা: \n";
+
+?>
